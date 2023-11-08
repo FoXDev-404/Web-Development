2d6c52bca248899fcf1235e2e2ef08b380f85c99
FoXDev-404 <rajpal010304@gmail.com>
2023-11-08 19:31:04 +0530

    Love Score Calculator

--- 14 - Intermediate Javascript/script.js
+++ 14 - Intermediate Javascript/script.js
@@ -8,3 +8,11 @@ n = n * 6;
 n = Math.floor(n);
 n = n + 1;
 console.log(n);
+
+//Love Score Calculator
+prompt("Enter first name : ");
+prompt("Enter second name : ");
+var loveScore = Math.random();
+loveScore = loveScore * 100;
+loveScore = Math.floor(loveScore) + 1;
+alert("The love score is " + loveScore + "%");
