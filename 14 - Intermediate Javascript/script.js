5014562d030b6c246923300010aa3037a1a2dcbf
FoXDev-404 <rajpal010304@gmail.com>
2023-11-08 19:32:45 +0530

    Init

--- 14 - Intermediate Javascript/script.js
+++ 14 - Intermediate Javascript/script.js
@@ -1,18 +1,22 @@
 // Simple Prompt and Alert program.
-prompt("Enter Your Name : ");
-alert("Hya there...");
+// prompt("Enter Your Name : ");
+// alert("Hya there...");
 
 // Random Number Generator.
+/*
 var n = Math.random();
 n = n * 6;
 n = Math.floor(n);
 n = n + 1;
 console.log(n);
+*/
 
 //Love Score Calculator
+/*
 prompt("Enter first name : ");
 prompt("Enter second name : ");
 var loveScore = Math.random();
 loveScore = loveScore * 100;
 loveScore = Math.floor(loveScore) + 1;
 alert("The love score is " + loveScore + "%");
+*/
