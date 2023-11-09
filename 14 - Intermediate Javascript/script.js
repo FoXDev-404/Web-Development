766626411a5c2d0b7fa0d4d4c85355d08bb74261
FoXDev-404 <rajpal010304@gmail.com>
2023-11-09 22:13:30 +0530

    Adding Elements and Intermediate Array Techniques

--- 14 - Intermediate Javascript/script.js
+++ 14 - Intermediate Javascript/script.js
@@ -100,3 +100,26 @@ if (guestList.includes(guestName)) {
   alert("Sorry, maybe next time.");
 }
 */
+
+// Adding Elements and Intermediate Array Techniques
+/*
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+  if (count % 3 === 0 && count % 5 === 0) {
+    output.push("FizzBuzz");
+  } else if (count % 3 === 0) {
+    output.push("Fizz");
+  } else if (count % 5 === 0) {
+    output.push("Buzz");
+  } else {
+    output.push(count);
+  }
+  count++;
+
+  console.log(output);
+}
+
+fizzBuzz();
+*/
