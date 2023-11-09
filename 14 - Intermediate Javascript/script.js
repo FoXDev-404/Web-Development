10b4e15575d42a06b2ddf9d96ad9f18bbd27d5e2
FoXDev-404 <rajpal010304@gmail.com>
2023-11-09 22:53:32 +0530

    Control Statement Using While Loop

--- 14 - Intermediate Javascript/script.js
+++ 14 - Intermediate Javascript/script.js
@@ -137,3 +137,25 @@ function whosPaying(names) {
 }
 whosPaying(names);
 */
+
+// Control Statements While Loops
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+  while (count <= 100) {
+    if (count % 3 === 0 && count % 5 === 0) {
+      output.push("FizzBuzz");
+    } else if (count % 3 === 0) {
+      output.push("Fizz");
+    } else if (count % 5 === 0) {
+      output.push("Buzz");
+    } else {
+      output.push(count);
+    }
+    count++;
+  }
+  console.log(output);
+}
+
+fizzBuzz(); // Function Call..
