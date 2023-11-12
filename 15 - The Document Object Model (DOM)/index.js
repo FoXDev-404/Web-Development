155edd77c34023a8ee6a46ea7e7757d0a2502120
FoXDev-404 <rajpal010304@gmail.com>
2023-11-12 21:10:28 +0530

    Change color of list items using DOM selectors

--- 15 - The Document Object Model (DOM)/index.js
+++ 15 - The Document Object Model (DOM)/index.js
@@ -1,3 +1,9 @@
 document.getElementsByTagName("li"); // HTMLCollection(3) [li, li, li]
 
-document.getElementsByTagName("li")[1].style.color = "red"; // red
+document.getElementsByTagName("li")[1].style.color = "green"; // green
+
+document.querySelectorAll("ul .item"); // NodeList(3) [li.item, li.item, li.item]
+
+document.querySelectorAll("ul .item")[0].style.color = "red"; // red
+
+document.querySelectorAll("ul .item")[2].style.color = "blue"; // blue
