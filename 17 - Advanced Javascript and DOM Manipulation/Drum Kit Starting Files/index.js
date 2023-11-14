b3f69d8aa0743ab882848ceba04c5e7f7dc3eba9
FoXDev-404 <rajpal010304@gmail.com>
2023-11-14 22:38:26 +0530

    Add drum sounds and images to buttons

--- 17 - Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
+++ 17 - Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
@@ -5,6 +5,9 @@
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
   // const element = array[i];
   document.querySelectorAll("button")[i].addEventListener("click", function () {
-    alert("I got clicked.");
+    // alert("I got clicked.");
+
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
   });
 }
