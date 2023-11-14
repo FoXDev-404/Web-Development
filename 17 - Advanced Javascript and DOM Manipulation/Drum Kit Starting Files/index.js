2cb16fa84250d361773e2d9188b7da5221c163aa
FoXDev-404 <rajpal010304@gmail.com>
2023-11-14 20:14:32 +0530

    Add event listener to drum buttons

---
+++ 17 - Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
@@ -0,0 +1,10 @@
+// document.querySelector("button").addEventListener("click", function () {
+//   alert("I got clicked.");
+// });
+
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  // const element = array[i];
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    alert("I got clicked.");
+  });
+}
