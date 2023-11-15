ceb80e08dc08d091b4fa637998f90521891ebc72
FoXDev-404 <rajpal010304@gmail.com>
2023-11-15 21:34:52 +0530

    Add support for uppercase letters in drum kit sounds

--- 17 - Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
+++ 17 - Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
@@ -16,38 +16,74 @@ document.addEventListener("keydown", function (event) {
 function makeSound(key) {
   switch (key) {
     case "w":
-      var audio = new Audio("sounds/tom-1.mp3");
-      audio.play();
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
       break;
 
     case "a":
-      var audio = new Audio("sounds/tom-2.mp3");
-      audio.play();
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
       break;
 
     case "s":
-      var audio = new Audio("sounds/tom-3.mp3");
-      audio.play();
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
       break;
 
     case "d":
-      var audio = new Audio("sounds/tom-4.mp3");
-      audio.play();
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
       break;
 
     case "j":
-      var audio = new Audio("sounds/snare.mp3");
-      audio.play();
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
       break;
 
     case "k":
-      var audio = new Audio("sounds/crash.mp3");
-      audio.play();
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
       break;
 
     case "l":
-      var audio = new Audio("sounds/kick-bass.mp3");
-      audio.play();
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    // For uppercase letters
+    case "W":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "A":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "S":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "D":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "J":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "K":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "L":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
       break;
 
     default:
