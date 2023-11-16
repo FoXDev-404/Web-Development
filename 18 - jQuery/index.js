11f26d88a15b4095895a9941dd056f4638d7dcd9
FoXDev-404 <rajpal010304@gmail.com>
2023-11-16 22:24:35 +0530

    Add jQuery element selection and manipulation examples.

--- 18 - jQuery/index.js
+++ 18 - jQuery/index.js
@@ -1,1 +1,21 @@
-$("h1").css("color", "red");
+// Selecting element in jQuery
+$("h1"); // Selects all h1 elements
+$("button"); // Selects all button elements
+
+// Selecting by ID and Class
+$("#heading"); // Selects the element with id="heading"
+$(".btn"); // Selects all elements with class="btn"
+
+// Manipulating Style
+$("h1").css("color", "blue"); // Changes the color of all h1 elements to blue
+
+// Manipulating Text
+$("h1").text("Bye"); // Changes the text of all h1 elements to Bye
+
+$("button").text("Don't Click Me"); // Changes the text of all button elements to Don't Click Me
+
+// Manipulating Attributes
+$("img").attr(
+  "src",
+  "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+); // Changes the source of all img elements to the Google logo
