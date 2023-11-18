2bf1b2700aa76b50c6954e2a545cf0365b27f5fc
FoXDev-404 <rajpal010304@gmail.com>
2023-11-19 00:27:13 +0530

    NPM package install and used

---
+++ 20 - Backend Node.js/20.3 NPM/index.js
@@ -0,0 +1,20 @@
+/* 
+// var generateName = require("sillyname");
+import generateName from "sillyname";
+
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
+*/
+
+/*
+// var generateName = require("superheroes");
+import generateName from "superheroes";
+// To generate a random name
+var myHeroName = generateName.random();
+console.log(`My hero name is ${myHeroName}.`);
+
+// To generate all the names
+var allHeroNames = generateName.all;
+console.log(allHeroNames);
+*/
