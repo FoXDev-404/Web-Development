587b33961aed395fdb3e51d12aee7154aa0c668c
FoXDev-404 <rajpal010304@gmail.com>
2023-12-31 22:37:37 +0530

    init

--- 23 - Challenge 3 - Blog Website/app.js
+++ 23 - Challenge 3 - Blog Website/app.js
@@ -1,5 +1,3 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -12,7 +10,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
@@ -28,6 +26,6 @@ app.use(express.static("public"));
 
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
